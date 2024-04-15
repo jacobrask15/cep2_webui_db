9f55e893f902b8d5b4af2741f201f1aacf47c1b9
jacobrask15 <49784405+jacobrask15@users.noreply.github.com>
2024-04-15 12:02:17 +0200

    centered body

--- index.php
+++ index.php
@@ -23,15 +23,15 @@
         <?php } ?>
 
             <div class="imgcontainer">
-        <img src="img_avatar2.png" alt="Avatar" class="avatar">
+        <img src="dawg.jpg" alt="Avatar" class="avatar">
     </div>
 
     <div class="container">
         <label for="uname"><b>Username</b></label>
         <input type="text" placeholder="Enter Username" name="uname" required>
 
-        <label for="Password"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="Password" required>
+        <label for="password"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="password" required>
 
         <button type="submit">Login</button>
     </div>
