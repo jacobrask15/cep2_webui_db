8e29328bcaf5ee5aca39da8d4c5a64b833b9592b
Christian <chrbp4@gmail.com>
2024-04-18 13:30:29 +0200

    new_data

--- mock_data.php
+++ mock_data.php
@@ -7,11 +7,13 @@ $conn = OpenCon();
 
 # YYYY-MM-DD hh:mm:ss
 
-for ($x = 0; $x <= 30; $x++) {
+for ($x = 0; $x <= 365; $x++) {
 $r = $x % 5;
+$day = $x % 30;
+$month = $x % 12;
 $m = $x % 100;
 $sqlquery = "INSERT INTO `event` (`timestamp_`, `loglevel`, `type_`, `device_id`, `device_type`, `measurement`) VALUES
-('2024-04-$x 00:23:25', 'Informational', 'movement', 'ROOM $r', 'PIR', '$m')";
+('2024-$month-$day 00:23:25', 'Informational', 'toilet', 'ROOM $r', 'PIR', '$m')";
 
 
 mysqli_query($conn, $sqlquery);
