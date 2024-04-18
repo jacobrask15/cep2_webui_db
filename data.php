89892c47c798b41f2e24915bf82a248a023daee1
Christian <chrbp4@gmail.com>
2024-04-18 09:55:29 +0200

    chart

---
+++ data.php
@@ -0,0 +1,21 @@
+<?php
+header('Content-Type: application/json');
+
+include "db_connection.php";
+
+$conn = OpenCon();
+
+$sqlQuery = "SELECT timestamp,loglevel,message FROM EVENT;
+
+$result = mysqli_query($conn,$sqlQuery);
+
+$data = array();
+foreach ($result as $row) {
+	$data[] = $row;
+}
+
+mysqli_close($conn);
+
+echo json_encode($data);
+?>
+
