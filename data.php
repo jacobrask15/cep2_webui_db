3e2c34e2d59b14b594f8d9a8fc78ce6b786522e6
Christian <chrbp4@gmail.com>
2024-04-18 10:33:30 +0200

    sidste

--- data.php
+++ data.php
@@ -5,7 +5,7 @@ include "db_connection.php";
 
 $conn = OpenCon();
 
-$sqlQuery = "SELECT timestamp, type FROM event;
+$sqlQuery = "SELECT device_id, type FROM event;
 
 $result = mysqli_query($conn,$sqlQuery);
 
